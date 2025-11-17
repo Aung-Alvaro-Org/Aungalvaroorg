bacd0092a0d28b906a8898628a09ecb404c9e477
Alvaro Balbin <kamthegod34@gmail.com>
2025-11-17 19:49:21 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ConfessionsSection } from "./components/ConfessionsSection";
 import { GuidelinesSection } from "./components/GuidelinesSection";
 import { Footer } from "./components/Footer";
 import { useState } from "react";
+import { Toaster } from "sonner";
+
 
 export default function App() {
   const [refreshTrigger, setRefreshTrigger] = useState(0);
@@ -14,6 +16,8 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-[#0a0a0f] dark">
+      <Toaster richColors position="top-center" />
+      
       <Navigation />
       <ConfessionHero onConfessionSubmitted={handleConfessionSubmitted} />
       <ConfessionsSection refreshTrigger={refreshTrigger} />
